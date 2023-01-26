15a482d6d88865a1bec83d6a1c5ce9c00efd9ba3
Chris2609 <megustas260904@gmail.com>
2023-01-26 12:38:04 +0100

    clase Conector creada y hecha

---
+++ src/biblioteca/Conector.java
@@ -0,0 +1,24 @@
+package biblioteca;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conector {
+		protected Connection con;
+		
+		private static final String HOST = "localhost";
+		private static final String BBDD = "biblioteca";
+		private static final String USERNAME = "root";
+		private static final String PASSWORD = "";
+		
+		public Conector() throws ClassNotFoundException, SQLException {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://"+HOST+"/"+BBDD, USERNAME, PASSWORD);
+
+		}
+		
+		public void cerrarConexion() throws SQLException {
+			con.close();
+		}
+}

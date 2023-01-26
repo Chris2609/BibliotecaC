ecd501b757bc592037a2f07755d1e7befa2a95b2
Chris2609 <megustas260904@gmail.com>
2023-01-26 13:03:52 +0100

    clase prestamo con sus atributos y getters setters

---
+++ src/biblioteca/Prestamo.java
@@ -0,0 +1,39 @@
+package biblioteca;
+
+import java.util.Date;
+
+public class Prestamo {
+
+	private int idLibro;
+	private int idSocio;
+	private Date fecha;
+	private boolean devuelto;
+	
+	public int getIdLibro() {
+		return idLibro;
+	}
+	public void setIdLibro(int idLibro) {
+		this.idLibro = idLibro;
+	}
+	public int getIdSocio() {
+		return idSocio;
+	}
+	public void setIdSocio(int idSocio) {
+		this.idSocio = idSocio;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public boolean isDevuelto() {
+		return devuelto;
+	}
+	public void setDevuelto(boolean devuelto) {
+		this.devuelto = devuelto;
+	}
+	
+	
+	
+}

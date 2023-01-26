78e4caf7f06af0513e716031d9b876e3a4a5ac4e
Chris2609 <megustas260904@gmail.com>
2023-01-26 12:26:11 +0100

    clas emenu hecha con sysouts

--- src/biblioteca/Menu.java
+++ src/biblioteca/Menu.java
@@ -1,55 +1,21 @@
 package biblioteca;
 
-import java.util.Scanner;
-
 public class Menu {
 	
 	public static void menuPrincipal() {
-		final int GESTIONAR_LIBROS = 1;
-        final int GESTIONAR_SOCIOS = 2;
-        final int GESTIONAR_PRESTAMOS = 3;
-        final int SALIR = 0;
-        Scanner scan = new Scanner(System.in);
-        int opcion_menu;
-        do {
-            System.out.println(" ------MENU------");
-            System.out.println(GESTIONAR_LIBROS + ". Gestionar libros");
-            System.out.println(GESTIONAR_SOCIOS + ". Gestionar Socios");
-            System.out.println(GESTIONAR_PRESTAMOS + ". Gestionar prestamos");
-            System.out.println(SALIR + ". Salir");
-            System.out.println("Elije una de las opciones");
-            opcion_menu = Integer.parseInt(scan.nextLine());
-            switch (opcion_menu) {
-            case GESTIONAR_LIBROS:
-                System.out.println("primera opcion seleccionada\n");
-                menuLibros();
-                break;
-            case GESTIONAR_SOCIOS:
-                System.out.println("segunda opcion seleccionada\n");
-                menuSocios();
-                break;
-            case GESTIONAR_PRESTAMOS:
-                System.out.println("tercera opcion seleccionada\n");
-                menuPrestamos();
-                break;
-            case SALIR:
-                System.out.println("ADIOS");
-                break;
-            default:
-                System.out.println("Opcion incorrecta!");
-            }
-        } while (opcion_menu != SALIR);
+		System.out.println("1. Gestionar Libros\n2. Gestionar Socios\n3. Gestionar Prestamos");
 	}
 	
 	public static void menuLibros() {
-		
+		System.out.println("1. Insertar libro\n2. Eliminar libro\n3. Visualizar libros");
 	}
 	
 	public static void menuSocios() {
-		
+		System.out.println("1. Ver socios");
 	} 
 	
 	public static void menuPrestamos() {
+		System.out.println("Realizar préstamo");
 	}
 
 }

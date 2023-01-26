c90796c5e0b66e05ef2984da5d837b6cfdb358fd
Chris2609 <megustas260904@gmail.com>
2023-01-26 12:49:06 +0100

    clse libro con atributos con getters y setters

---
+++ src/biblioteca/Libro.java
@@ -0,0 +1,37 @@
+package biblioteca;
+
+public class Libro {
+
+	private int id;
+	private String titulo;
+	private String autor;
+	private int numPag;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public int getNumPag() {
+		return numPag;
+	}
+	public void setNumPag(int numPag) {
+		this.numPag = numPag;
+	}
+	
+	
+	
+}

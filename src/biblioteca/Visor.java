410fdf543474ed5e372575ba78e05bf74a817168
Chris2609 <megustas260904@gmail.com>
2023-01-30 13:09:22 +0100

    clase visor con metodos vacios

---
+++ src/biblioteca/Visor.java
@@ -0,0 +1,19 @@
+package biblioteca;
+
+import java.util.ArrayList;
+
+public class Visor {
+
+	public static void mostrarLibros(ArrayList<Libro> libros) {
+		
+	}
+	
+	public static void mostrarLibro(Libro libro) {
+		
+	}
+	
+	public static void mostrarMensaje(String mensaje) {
+		
+	}
+	
+}

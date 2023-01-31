6bf98932af58a575596282f110e391f47e97bfb0
Chris2609 <megustas260904@gmail.com>
2023-01-31 08:24:08 +0100

    formulariosDeDatos con modificarDatosLibro y pedirIdLibro

--- src/biblioteca/FormulariosDeDatos.java
+++ src/biblioteca/FormulariosDeDatos.java
@@ -14,14 +14,29 @@ public class FormulariosDeDatos {
 		System.out.println("Introduce el número de páginas");
 		libro.setNumPag(Integer.parseInt(scan.nextLine()));
 		
-		return null;
+		return libro;
 	}
 	
 	public static Libro modificarDatosLibro(Libro libro, Scanner scan) {
 		
+		System.out.println("Introduce el nuevo titulo del libro");
+		libro.setTitulo(scan.nextLine());
+		System.out.println("Introduce el nuevo autor del libro");
+		libro.setAutor(scan.nextLine());
+		System.out.println("Introduce e nuevo número de páginas del libro");
+		libro.setNumPag(Integer.parseInt(scan.nextLine()));
 		
+		return libro;
+	}
+	
+	public static int pedirIdLibro(Scanner scan) {
 		
-		return null;
+		int idLibro;
+		System.out.println("Introduce el ID del libro");
+		idLibro = Integer.parseInt(scan.nextLine());
+		
+		return 0;
+		
 	}
 	
 }
